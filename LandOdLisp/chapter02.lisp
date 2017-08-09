98a2212698fd3f4a58cd8103213a1c6088b80deb
nachoba <0800nacho@gmail.com>
2017-08-09 11:28:55 -0300

    added Land

---
+++ LandOdLisp/chapter02.lisp
@@ -0,0 +1,8 @@
+;; Chapter 02 :: Creating Your First Lisp Program
+;; ----------
+
+#| This should be a multiline comment in Common Lisp
+   which eventually should not be evaluated by the compiler
+|#
+(defun square(x)
+  (* x x))
