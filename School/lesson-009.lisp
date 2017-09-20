2ecc8c1f2492df91cd8925e49bc8de2a447c4e10
Ignacio Sniechowski <0800nacho@gmail.com>
2017-09-20 12:30:14 -0300

    finished lesson 9

--- School/lesson-009.lisp
+++ School/lesson-009.lisp
@@ -4,26 +4,30 @@
     author:      ignacio sniechowski
     mail:        0800nacho@gmail.com
     date:        19/09/2017
-    revision:    
+    revision:    20/09/2017  
     description: Chapter 5 from "Land of Lisp"
                  In this chapter we begin the building of a text game
                  engine.
 
-    contents:    [1] Introduction
-                 [2] Describing the Scenery with an Association List
-                 [3] Describing the Location
-                 [4] Describing the Paths
-                 [5] How Quasiquoting Works
-                 [6] Describing Multiple Paths at Once
-                 [7] Describing Objects at a Specific Location
-
-
-
+    contents:    [01]  Introduction
+                 [02]  Describing the Scenery with an Association List
+                 [03]  Describing the Location
+                 [04]  Describing the Paths
+                 [05]  How Quasiquoting Works
+                 [06]  Describing Multiple Paths at Once
+                 [07]  Describing Objects at a Specific Location
+                 [08]  Describing Visible Objects
+                 [09]  Describing it All
+                 [10]  Walking Around in our World
+                 [11]  Picking Up Objects
+                 [12]  Checking Our Inventory
+                 [13]  Summary
+ 
 |#
 
 #|
 
-    [1] INTRODUCTION
+    [01] INTRODUCTION
     In this lesson we introduce "The Wizard's Adventure Game", in which you
     are a wizard's apprentice. You'll explore the wizard's house.  
     Our game world consists of:
@@ -52,7 +56,7 @@
     * Objects that you can pick up and manipulate
     
 
-    [2] DESCRIBING THE SCENERY WITH AN ASSOCIATION LIST
+    [02] DESCRIBING THE SCENERY WITH AN ASSOCIATION LIST
     The world inside our adventure game is very simple, contains only three
     locations. Let's create a top-level variable, "*nodes*", to contain
     descriptions of the locations that exist in our game:
@@ -83,7 +87,7 @@
 
 
 
-    [3] DESCRIBING THE LOCATION
+    [03] DESCRIBING THE LOCATION
     Now that the "alist" of our game world is created, we need to define a command
     to describe a location. To do this, we will use the "assoc" funtion to find the
     correct item in the list using a key:
@@ -115,7 +119,7 @@
     can write code that can easily be tested in isolation.
 
     
-    [4] DESCRIBING THE PATHS
+    [04] DESCRIBING THE PATHS
     Now that we have descriptions of each location, we need descriptions of paths
     to other locations as well. We will create a second variable "*edges*", that
     contains the paths that players can take to move between places on our map.
@@ -150,7 +154,7 @@
     us to create chunks of data that have small pieces of Lisp code embedded in them.
 
 
-    [5] HOW QUASIQUOTING WORKS
+    [05] HOW QUASIQUOTING WORKS
     To enable quasiquoting, you must use a backquote [`] and not a single quote [']
     when switching from code to data mode. The "describe-path" function has just
     such a backquote in it.
@@ -168,7 +172,7 @@
     Lisp attempts to make list manipulation as easy as possible.
 
 
-    [6] DESCRIBING MULTIPLE PATHS AT ONCE
+    [06] DESCRIBING MULTIPLE PATHS AT ONCE
     Now let's use our "describe-path" function to create a more advanced function.
     Since a location may have any number of paths exiting from it, we need a function
     that can generate descriptions for all edges from a given location by looking up
@@ -327,7 +331,7 @@
 
 
 
-    [7] DESCRIBING OBJECTS AT A SPECIFIC LOCATION
+    [07] DESCRIBING OBJECTS AT A SPECIFIC LOCATION
     To create the final piece of code to help us visualize our game world, we need
     to describe the objects on the floor at a given location, which a player can pick
     up and use.
@@ -355,7 +359,7 @@
 
 |#
 
-(defunc objects-at (loc objs obj-locs)
+(defun objects-at (loc objs obj-locs)
   (labels ((at-loc-p (obj)                             ;; (1)
 	     (eq (cadr (assoc obj obj-locs)) loc)))    ;; (2)
     (remove-if-not #'at-loc-p objs)))                  ;; (3)
@@ -387,4 +391,251 @@
     Here is what the "object-at" looks like in action:
 
     > (objects-at 'living-room *objects* *object-locations*)
+    (WHISKEY BUCKET)
+
+
+    [08] DESCRIBING VISIBLE OBJECTS
+    Now we can write a function to describe the objects visible at a given location:
+
+|#
+
+(defun describe-objects (loc objs obj-loc)
+  (labels ((describe-obj (obj)                                                 ;; (1)
+	     `(you see a ,obj on the floor.)))                                 ;; (2)
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))   ;; (3)
+
+#|
+
+    "describe-objects" first creates the "describe-obj" function (1). This function
+    generates a pretty sentence stating that a given object is on the floor, using
+    quasiquoting (2).
+    The main part of the function consists of calling "objects-at" to find the objects
+    at the current location, mapping "describe-obj" across this list of objects, and
+    finally appending the descriptions into a single list (3).
+
+    Let's try running "describe-objects":
+
+    > (describe-objects 'living-room *objects* *object-location*)
+    (YOU SEE A WHISKEY ON THE FLOOR. YOU SEE A BUCKET ON THE FLOOR.)
+
+
+    [09] DESCRIBING IT ALL
+    Now we'll tie all of these description functions into one easy command called "look".
+    Because this will be the actual command players can enter to look around them in the
+    game, "look" will need to know a player's current location. 
+
+    So, we need a variable to track the player's current position; we will call this
+    variable *location*
+|#
+
+(defparameter *location* 'living-room)
+
+#|
+
+    The *location* value is initialized to the "living-room" symbol, which occurs at the
+    very start of the game. So players will find themselves in the living room at the
+    beginning of the game.
+
+    At this point we can write a "look" function to describe everything we need by having
+    it call of our descriptor functions:
+
+|#
+
+(defun look ()
+  (append (describe-location *location* *nodes*)
+	  (describe-paths *location* *edges*)
+	  (describe-objects *location* *objects* *object-locations*)))
+
+#|
+
+    Notice that the "look" function use global variables (such as *location*, *nodes*, etc).
+    This means that the player won't have to pass any values in order to look out the world.
+    However, this also means that the "look" function is not in the functional programming
+    style, because functions in this style reference only parameters or variables declared
+    in the function itself.
+
+    Since the player's location changes as the game progresses, "look" will do "different
+    things" at "different times" in the game. The things you see when looking around will
+    change depending on your location.
+
+    In contrast, a function in the functional programming style always returns the same
+    result, as long as the same values are given as parameters.
+
+    Let's see "look" in action:
+
+    > (look)
+    (YOU ARE IN A LIVING-ROOM.
+    A WIZARD IS SNORING LOUDLY ON THE COUCH.
+    THERE IS A DOOR GOING WEST FROM HERE. 
+    THERE IS A LADDER GOING UPSTAIRS FROM HERE. 
+    YOU SEE A WHISKEY ON THE FLOOR. 
+    YOU SEE A BUCKET ON THE FLOOR.)
+
+
+    [10] WALKING AROUND IN OUR WORLD
+    Now that we can see things in our world, let's write some code so that we can walk
+    around. The "walk" function (not in the functional style) takes a direction and lets
+    us walk there:
+
+|#
+
+(defun walk (direction)
+  (let ((next (find direction                                          ;; (1)
+		    (cdr (assoc *location* *edges*))                   ;; (2)
+		    :key #'cadr)))                                     ;; (3)
+    (if next                                                           ;; (4)
+	(progn (setf *location* (car next))                            ;; (5)
+	       (look))                                                 ;; (6)
+	'(you cannot go that way.))))                                  ;; (7)
+
+#|
+
+    First, this function looks up the available walking paths in the *edges* table,
+    using the current location (2). This is used by the "find" function to locate the
+    path marked with the appropriate direction (1).
+    "find" searches a list for an item, then returns that found item). The "direction"
+    (such as "west", "upstairs", and so on) will be in the cadr of each path, so we 
+    need to tell "find" to match the "direction" against the "cadr" of all the paths in
+    the list.
+
+    We can do this by passing "find" a keyword parameter (3). In Common Lisp, many
+    functions (such as "find") have built-in features that can be accessed by passing
+    in special parameters at the end of the function call. For instance, the following
+    code finds the first item in a list that has the symbols "y" in the "cadr" location:
+
+    > (find 'y '((5 x) (3 y) (7 z)) :key #'cadr)
+    (3 Y)
+
+    A keyword parameter has two parts:
+
+    * The first is the name (in this case :key), which begins with a colon
+    * The second is the value, which in this case is the #'cadr
+
+    We use keyword parameters the same way in our "walk" function to find the
+    proper path based on the given direction.
+    Once we have the correct path, we store the result in the variable "next" (1).
+    The "if" expression then checks whether "next" has a value (4), that is, the "next"
+    variable isn't "nil". If "next" has a value, "if" adjusts the player's position
+    because this is a valid direction (5). The call to "look" (6) retrieves the 
+    description for the new location and returns it as a value. If the player chooses
+    an invalid direction, "look" will generate an admonishment instead of a new
+    description (7).
+
+    Let's see our "walk" function in action:
+
+    > (walk 'west)
+    (YOU ARE IN A BEAUTIFUL GARDEN. 
+    THERE IS A WELL IN FRON OF YOU. 
+    THERE IS A DOOR GOING EAST FROM HERE. 
+    YOU SEE A FROG ON THE FLOOR. 
+    YOU SEE A CHAIN ON THE FLOOR.)
+
+    There's a quote in front of the direction, since the direction name needs to
+    be written in data mode. It's awkward to force a player to put a quote in a
+    game command.
+
+
+    [11] PICKING UP OBJECTS
+    Next, let's create a command to pick up objects in our world. To do so, we modify
+    the variable *object-locations* that we're using to track the location of objects:
+|#
+
+(defun pickup (object)
+  (cond ((member object                                                  ;; (1)
+		 (objects-at *location* *objects* *object-locations*))   ;; (2)
+	 (push (list object 'body) *object-locations*)                   ;; (3)
+	 `(you are now carrying the ,object))
+	(t '(you cannot get that.))))
+
+#|
+
+    The "pickup" function uses the "member" (1) function to see if the "object" is
+    indeed on the floor of the current location.
+    The "member" function checks to see if a particular item is found in a list of items.
+    We use the "objects-at" command (2) to generate the lists of objects at the current
+    location.
+    If the object is at the current location, we use the "push" command (3) to push a
+    new item onto the *object-locations* list, consisting of the item and its new
+    location. The new location will just be "body", for the player's body.
+    The "push" command (3) simply adds a new item to the front of a list variable's list.
+    For example, the following example adds the number 7 to the list 1 2 3:
+
+    > (defparameter *foo* '(1 2 3))
+    *FOO*
+    > (push 7 *foo*)
+    (7 1 2 3)
+
+    The "push" command is basically a convenience function built on top of "setf". For
+    example, we could have replaced the preceding "push" command with:
+    
+    > (setf *foo* (cons 7 *foo*))
+    
+    And obtained the same result, but it's easier to use "push".
+
+    Pushing a new location for an object onto our *object-locations* alist seems a bit
+    odd. Since we're never removing old locations for objects, just pushing new ones,
+    it means that *object-locations* may contain multiple entries for a single object,
+    and that this list now has two stored locations for the object in question.
+    Fortunately, the "assoc" command, which we use to find objects in a given location (
+    whithing the "objects-at" command), always returns the first item it finds in a list.
+    Therefore, using the "push" command makes the "assoc" command behave as if the value
+    in the list for a given key has been replaced altogether.
+
+
+    Using the "push" and "assoc" commands together in this way allows us to pretend that
+    values in an alist are changing, while still preserving old values. Old values are
+    simply suppressed by newer values, thus preserving a history of all old values.
+    The "push"/"assoc" idiom is a common technique used by lispers.
+
+    Now let's walk back to the living room and try to pick up an object:
+
+    > (walk 'east)
+    (YOU ARE IN A LIVING-ROOM. 
+    A WIZARD IS SNORING LOUDLY ON THE COUCH. 
+    THERE IS A DOOR GOING WEST FROM HERE. 
+    THERE IS A LADDER GOING UPSTAIRS FROM HERE. 
+    YOU SEE A WHISKEY ON THE FLOOR. 
+    YOU SEE A BUCKET ON THE FLOOR.)
+
+
+    > (pickup 'whiskey)
+    (YOU ARE NOW CARRYING THE WHISKEY)
+
+
+    [12] CHECKING OUR INVENTORY
+    Finally, let's create a function that lets players see an inventory of objects they
+    are carrying:
+    
+|#
+
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+#|
+
+    This inventory function uses the "object-at" function to retrieve a list of objects
+    at a requested location. What location does it search for?
+    If you remember, when an object was picked up by the player, we changed its location 
+    to "'body". This is the location we now use to query:
+
+    Let's try out this "inventory" function:
+
+    > (inventory)
+    (ITEMS- WHISKEY)
+
+
+   [13] SUMMARY
+   * A game world can be represented by a mathematical graph, consisting of "nodes" for the
+     places the player can visit and "edges" for the paths between these places.
+   * You can store these nodes in an association list "alist" called *nodes*. This alist
+     allows you to look up properties of a node/place by using its name. In the case of our
+     game, the property we're storing is a description of each node/place.
+   * You use the "assoc" function to look up a key (location name in our example) in an alist.
+   * "Quasiquoting" is a technique that allows you to insert small bits of computer code into
+     larger pieces of data.
+   * Some Lisp functions accept other functions as arguments. These are called higher-order
+     functions. The "mapcar" function is the most popular higher-order function in CL.
+   * To replace a value from an alist, you "push" new items onto the list. Only the most
+     recent value will be reported by the "assoc" function.
+
 |#
